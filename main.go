3ef2901060050b89d0b6d44b92d5418a917f7c36
jasper <jasper@localhost.localdomain>
2020-07-19 20:56:37 +0700

    add module

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "devsecops/module"
 )
 
 // Calculate returns x + 2.
@@ -12,4 +13,5 @@ func Calculate(x int) (result int) {
 
 func main() {
     fmt.Println("Hello World")
+    module.Mo()
 }
---
+++ module/module.go
@@ -0,0 +1,7 @@
+package module
+
+import "fmt"
+
+func Mo(){
+	fmt.Println("Module")
+}
